d46f9b73aced23e54dc75cfb4fdc9077a8630c3e
Julita <254764@student.pwr.edu.pl>
2022-01-21 11:48:08 +0100

    Added 2 sout

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -1,5 +1,7 @@
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hi!");
+        System.out.println("Hi 2!");
     }
 }
